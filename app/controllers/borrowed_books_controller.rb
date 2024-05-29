9dfc8d8fa4a0527776ab08d6a40939726a383633
Bohdan Kurila <kuzmin2021ks11@student.karazin.ua>
2024-05-30 01:29:30 +0300

    Fix conflict borrowed_books_controller.rb

--- app/controllers/borrowed_books_controller.rb
+++ app/controllers/borrowed_books_controller.rb
@@ -4,25 +4,25 @@ class BorrowedBooksController < InheritedResources::Base
     @book = @borrowed_book.book
     library_card = current_client.library_card
 
-    if library_card.can_borrow_more_books? && @book.book_amount.positive? && @borrowed_book.save
-      @book.update(book_amount: @book.book_amount - 1)
+    return unless library_card.can_borrow_more_books? && @book.book_amount.positive? && @borrowed_book.save
 
-      if library_card.can_borrow_more_books? && @borrowed_book.save
-        Rails.logger.debug @book.book_amount
-        Rails.logger.debug
-        if @book.book_amount > 1
-          @book.update(book_amount: @book.book_amount - 1)
-        else
-          @book.update(book_amount: 0)
+    @book.update(book_amount: @book.book_amount - 1)
 
-        end
-        Rails.logger.debug @book.book_amount
-        Rails.logger.debug
-        redirect_to book_path(@book), notice: 'Книгу успішно позичено'
+    if library_card.can_borrow_more_books? && @borrowed_book.save
+      Rails.logger.debug @book.book_amount
+      Rails.logger.debug
+      if @book.book_amount > 1
+        @book.update(book_amount: @book.book_amount - 1)
       else
-        flash.now[:alert] = 'Не вдалося позичити книгу. Можливо, ви досягли ліміту запозичень або книги вже немає в наявності.'
-        redirect_to book_path(@book)
+        @book.update(book_amount: 0)
+
       end
+      Rails.logger.debug @book.book_amount
+      Rails.logger.debug
+      redirect_to book_path(@book), notice: 'Книгу успішно позичено'
+    else
+      flash.now[:alert] = 'Не вдалося позичити книгу. Можливо, ви досягли ліміту запозичень або книги вже немає в наявності.'
+      redirect_to book_path(@book)
     end
   end
 
