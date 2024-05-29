a54131b24930b5a25291fd7c70c1acdc26b51094
dennyKML <denis.kimlyk.of@gmail.com>
2024-05-29 13:49:39 +0300

    Improve client profile: add creation and viewing of library card

--- app/controllers/library_cards_controller.rb
+++ app/controllers/library_cards_controller.rb
@@ -1,4 +1,10 @@
 class LibraryCardsController < InheritedResources::Base
+  def create
+    @library_card = LibraryCard.new(library_card_params)
+    @library_card.save
+    redirect_to clients_profile_path(tab: 'card')
+  end
+
   private
 
   def library_card_params
